da5f0016cc05117fe59509761c2eb17a5679a365
Gusti Cahya <gusticahyax@yahoo.com>
2022-02-27 13:07:52 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        
+      </Router>
+    </div>
+  )
+}
+export default App
